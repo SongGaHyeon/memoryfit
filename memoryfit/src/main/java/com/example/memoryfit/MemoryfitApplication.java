e10b943c41a2b346b1c9b5d133880e2c9a3ce152
SongGaHyeon <gahyunsong@hufs.ac.kr>
2024-06-29 01:23:27 +0900

    project생성

---
+++ memoryfit/src/main/java/com/example/memoryfit/MemoryfitApplication.java
@@ -0,0 +1,13 @@
+package com.example.memoryfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryfitApplication.class, args);
+	}
+
+}
---
+++ memoryfit/src/test/java/com/example/memoryfit/MemoryfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memoryfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
